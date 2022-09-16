4765d7c2650d74dcfaeacda74796b32d357b58f0
ykxVK8yL5L <58539637+ykxVK8yL5L@users.noreply.github.com>
2022-09-16 19:33:18 +0800

    Update mongodb.py

--- mongodb.py
+++ mongodb.py
@@ -2,7 +2,7 @@ import certifi
 import pymongo
 import sys
 import argparse
-
+from bson.json_util import dumps, 
 
 parser = argparse.ArgumentParser(description='Insert data to mongdb.net')
 parser.add_argument("--con", help="Connection url", default="")
@@ -13,10 +13,22 @@ parser.add_argument("--size", help="Insert size", default="")
 args = parser.parse_args()
 
 
+if args.cid is None:
+  print("Nothing to save")
+  quit()
+
+
 client = pymongo.MongoClient(args.con, tlsCAFile=certifi.where())
 mydb = client["mydb"]
 mycol = mydb["web3"]
+taskcol = mydb["task"]
+
 
 mydict = {"name":args.name,"cid": args.cid,"size": args.size,"issync":0}
 x = mycol.insert_one(mydict)
+
+keyword="##"+args.name
+task = taskcol.find_one({'url':{'$regex':keyword}})
+taskcol.delete_one(task)
+
 print(x)
